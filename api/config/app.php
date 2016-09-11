3d929219ecf63cba548f03bb0ae654dc1008dee7
Mateusz Kasprzak <kacpak@gmail.com>
2016-09-11 13:19:46 +0200

    Added CORS

--- api/config/app.php
+++ api/config/app.php
@@ -179,6 +179,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Laravel\Passport\PassportServiceProvider::class,
+        Barryvdh\Cors\ServiceProvider::class,
     ],
 
     /*
