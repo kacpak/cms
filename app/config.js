e2b99653939375007e229195fd12004e12f975d3
Mateusz Kasprzak <kacpak@gmail.com>
2016-10-08 11:55:52 +0000

    Added API_ENDPOINT environmental variable

--- app/config.js
+++ app/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteTitle: 'kasprzakCMS Demo',
-  apiEndpoint: 'http://api.cms.dev',
+  apiEndpoint: process.env.API_ENDPOINT || 'http://api.cms.dev',
   clientId: '1',
   clientSecret: 'wupuUfoaaIseyxR9jxvCqI2NtfPz2LEY5djEG1jw'
 };
