4858404bd91337e80bc8c271a7b242f558ab1dc5
Mateusz Kasprzak <kacpak@gmail.com>
2016-11-18 14:48:02 +0100

    Added confirmation dialog during news deletion

--- app/src/app/admin/news/list/list-news.component.ts
+++ app/src/app/admin/news/list/list-news.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit, OnDestroy} from '@angular/core'
 import {News} from "../../../../typings/responses/responses";
 import {NewsService} from "../../../api/services/news.service";
 import {Subscription} from "rxjs";
+import {Modal} from "../../../shared/modal-util/modal-util";
 
 @Component({
   selector: 'list-news',
@@ -24,9 +25,24 @@ export class ListNewsComponent implements OnInit, OnDestroy {
   }
 
   onDelete(id: number, fieldset?: HTMLElement) {
-    jQuery(fieldset).attr("disabled", "disabled");
-    this.newsService.deleteNews(id).subscribe((isDeleted: boolean) => {
-      this.newsService.getNews().subscribe(news => this.newsArray = news as News[]);
-    });
+    let news = this.newsArray.find((news: News) => news.id == id);
+
+    Modal.getDangerDialog()
+        .content(`Czy na pewno chcesz usunąć news "${news.title}"?`)
+        .header('Usuwanie')
+        .onResolve(() => {
+          if (fieldset) {
+            jQuery(fieldset).attr('disabled', 'disabled');
+          }
+          this.newsService.deleteNews(id).subscribe(
+              (isDeleted: boolean) => {
+                this.newsService.getNews().subscribe(news => this.newsArray = news as News[]);
+              },
+              (error: any) => {
+                jQuery(fieldset).removeAttr('disabled');
+              }
+          );
+        })
+        .show();
   }
 }
